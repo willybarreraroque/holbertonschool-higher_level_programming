94cf81cfd13d1c9e3fd6f42e4f96431bfa2b9b3e
willybarreraroque <willy.br2002@gmail.com>
2023-11-06 14:55:44 -0800

    Task:0-print-special-character

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """
-Description: print text with special caracter
+Description: print text with special character
 """
 print("\"Programming is like buildig a multilingual puzzle\n")

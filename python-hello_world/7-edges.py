01a3fb7439c23bb9a8a425bfb5859901767ddffb
willybarreraroque <willy.br2002@gmail.com>
2023-11-09 21:28:50 -0800

    Task:05-print-arrays

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[0:3]
-word_last_2 = word[7:101010101010101010]
+word_last_2 = word[7:10]
 middle_word = word[1:8]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
